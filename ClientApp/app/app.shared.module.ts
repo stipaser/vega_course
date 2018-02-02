88a2db60b6df4abdc1e80d9a83c76a15fa57e55c
Sergiu Stipanov <sergiu.stipanov@outlook.com>
2018-02-02 10:01:53 +0200

    change vehicle-form location

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -10,7 +10,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
-import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
     declarations: [
--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,4 +1,4 @@
-import { MakeService } from './../services/make.service';
+import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
